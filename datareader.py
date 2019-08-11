a2e698d811680628d41cb21c495981ce9a750d72
Jimmy <z5163448@cse.unsw.edu.au>
2019-08-11 18:16:18 +1000

    changing to a scraping method to extract data

--- datareader.py
+++ datareader.py
@@ -1,18 +1,20 @@
-import datetime as dt
+from bs4 import BeautifulSoup
+from datetime import datetime as dt
 import matplotlib.pyplot as plt
 from matplotlib import style
 import pandas as pd
 import pandas_datareader.data as web
+import os
+import re
+import requests
+from time import mktime
 
 class backtest_database:
-    def __init__(self, ticker, start, end):
+    def __init__(self, ticker, start, end, interval):
         self.ticker = ticker
         self.start = start
         self.end = end
-
-    def create_csv(self):
-        df = web.get_data_yahoo(self.ticker, self.start, self.end)
-        df.to_csv(self.ticker + '.csv')
+        self.interval = interval
 
     def set_start_date(self, newStartDate):
         self.start = newStartDate
@@ -22,3 +24,35 @@ class backtest_database:
 
     def read_csv(self):
         return pd.read_csv(self.ticker + '.csv')
+
+    # WEBSCRAPING FUNCTIONS
+    def _get_crumbs_and_cookies(self):
+        url = 'https://finance.yahoo.com/quote/{}/history'.format(self.ticker)
+        with requests.session():
+            header = {'Connection': 'keep-alive',
+                   'Expires': '-1',
+                   'Upgrade-Insecure-Requests': '1',
+                   'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) \
+                   AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36'
+                   }
+            website = requests.get(url, headers=header)
+            soup = BeautifulSoup(website.text, 'lxml')
+            crumb = re.findall('"CrumbStore":{"crumb":"(.+?)"}', str(soup))
+            return (header, crumb[0], website.cookies)
+
+    def convert_to_unix(self, date):
+        datum = dt.strptime(date, '%Y-%m-%d').timetuple()
+        return int(mktime(datum))
+
+    def create_csv(self):
+        day_begin_unix = self.convert_to_unix(self.start)
+        day_end_unix = self.convert_to_unix(self.end)
+        header, crumb, cookies = self._get_crumbs_and_cookies()
+        # print(crumb)
+        with requests.session():
+            url = 'https://query1.finance.yahoo.com/v7/finance/download/' \
+                  '{stock}?period1={day_begin}&period2={day_end}&interval={interval}&events=history&crumb={crumb}' \
+                  .format(stock=self.ticker, day_begin=day_begin_unix, day_end=day_end_unix, interval=self.interval, crumb=crumb)
+            print(url)
+            website = requests.get(url, headers=header, cookies=cookies)
+            website.text.split('\n')[:-1]
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from datareader import *
 
 def main():
-	CBA = backtest_database('CBA.AX','1998-1-1','2019-1-1')
+	CBA = backtest_database('CBA.AX','2018-01-01','2019-1-1',1)
 	CBA.create_csv()
-	df_CBA = CBA.read_csv()
-	print(df_CBA['High'])
+	# df_CBA = CBA.read_csv()
+	# print(df_CBA)
 
 if __name__ == "__main__":
 	main()
