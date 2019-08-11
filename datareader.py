f219873191d87c4189c3e18a3cc48c3576db1b2a
Jimmy <z5163448@cse.unsw.edu.au>
2019-08-11 16:57:54 +1000

    created class and generic functions

--- datareader.py
+++ datareader.py
@@ -4,9 +4,21 @@ from matplotlib import style
 import pandas as pd
 import pandas_datareader.data as web
 
-def get_csv():
-    stock = 'CBA.AX'
-    start = '1992-01-01'
-    end = dt.date.today();
-    df = web.get_data_yahoo(stock, start, end)
-    df.to_csv('CBA.csv')
+class backtest_database:
+    def __init__(self, ticker, start, end):
+        self.ticker = ticker
+        self.start = start
+        self.end = end
+
+    def create_csv(self):
+        df = web.get_data_yahoo(self.ticker, self.start, self.end)
+        df.to_csv(self.ticker + '.csv')
+
+    def set_start_date(self, newStartDate):
+        self.start = newStartDate
+
+    def set_end_date(self, newEndDate):
+        self.end = newEndDate
+
+    def read_csv(self):
+        return pd.read_csv(self.ticker + '.csv')
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from datareader import *
 
 def main():
-	get_csv()
+	CBA = backtest_database('CBA.AX','1998-1-1','2019-1-1')
+	CBA.create_csv()
+	df_CBA = CBA.read_csv()
+	print(df_CBA['High'])
 
 if __name__ == "__main__":
 	main()
