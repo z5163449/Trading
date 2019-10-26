a68d3e71183657e0085884e8fa4bba5f49e843d9
Jimmy <z5163448@cse.unsw.edu.au>
2019-10-26 01:16:48 -0700

    non-linear regression applied

--- leastSquares.py
+++ leastSquares.py
@@ -3,14 +3,28 @@ import numpy as np
 # Current model can only take in a nx1 matrix
 # I need to work out how to make numpy work with 1d array as well as 2d
 class leastSquaresModel:
-    def __init__(self,X,y):
+    def __init__(self,X,y,p,k):
         (n,d) = np.shape(X)
         Z = np.ones((n,1))
         Z = np.hstack((Z,X))
+        # Changing features into polynomials
+        for i in range(2,p+1):
+            Z = np.hstack((Z,np.power(X,i)))
+        # Sine manipulation on features
+        for i in range(2,k+1):
+            Z = np.hstack((Z,np.sin(X*i)))
         self.w = np.linalg.solve(Z.T@Z,Z.T@y)
+        self.p = p
+        self.k = k
 
     def predict(self,Xhat):
         (n,d) = np.shape(Xhat)
         Z = np.ones((n,1))
         Z = np.hstack((Z,Xhat))
+        # Changing features into polynomial
+        for i in range(2,self.p+1):
+            Z = np.hstack((Z,np.power(Xhat,i)))
+        # Sine manipulation on features
+        for i in range(2,self.k+1):
+            Z = np.hstack((Z,np.sin(Xhat*i)))
         return Z@self.w
--- main.py
+++ main.py
@@ -11,12 +11,12 @@ def main():
 	# Load all asx200 companies as csv files
 	# df_asx200 = pd.read_csv('asx200_list.csv')
 	# for code in df_asx200['Code']:
-	# 	tmp = backtest_database(code + '.AX','2017-01-30','2019-08-11',1)
+	# 	tmp = backtest_database(code + '.AX','2018-01-30','2019-08-11',1)
 	# 	tmp.create_csv()
 
-	CBA = backtest_database('CBA.AX','2017-01-30','2019-08-11',1)
-	df_CBA = CBA.read_csv()
-	linearOLS(df_CBA)
+	ANZ = backtest_database('ANZ.AX','2018-01-30','2019-08-11',1)
+	df_ANZ = ANZ.read_csv()
+	linearOLS(df_ANZ)
 
 
 # Simple regression using date as parameter and Adj close as output\
@@ -30,16 +30,20 @@ def linearOLS(df):
 	ytrain = y[0:slice]
 	Xtest = X[slice:len(X)]
 	ytest = y[slice:len(y)]
-	regress = lsModel.leastSquaresModel(Xtrain,ytrain)
+	regress = lsModel.leastSquaresModel(Xtrain,ytrain,3,5)
 	yhat_train = regress.predict(Xtrain)
+	# print((yhat_train - ytrain))
 	testError = E((yhat_train - ytrain)**2)
 	print("Training Error =", testError)
 	yhat = regress.predict(Xtest)
 	testError = E((yhat - ytest)**2)
 	print("Training Error =", testError)
 	yplot = np.vstack((yhat_train, yhat))
-	df.plot(kind='line', x ='Date', y = 'Adj Close', color='blue')
-	plt.plot(df['Date'], yplot, label='Regression', color='green')
+	plt.plot(df['Date'], y, label='Stock Price', color='blue')
+	# Plots Training
+	plt.plot(df['Date'][:slice], yhat_train, label='Training', color='green')
+	# Plots Testing
+	# plt.plot(df['Date'][slice:], yhat, label='Testing', color='red')
 	plt.legend()
 	plt.show()
 
