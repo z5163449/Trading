ef6110080da187650c7f569b1687df5500898588
Jimmy <z5163448@cse.unsw.edu.au>
2020-01-08 11:31:29 +0800

    added loaddf function

--- leastSquares.py
+++ leastSquares.py
@@ -1,7 +1,10 @@
 import numpy as np
+from misc import *
+import matplotlib.pyplot as plt
 
 # Current model can only take in a nx1 matrix
 # I need to work out how to make numpy work with 1d array as well as 2d
+# As in I am currently only taking in 1 variable of input e.g. only taking in price for X
 class leastSquaresModel:
     def __init__(self,X,y,p,k):
         (n,d) = np.shape(X)
@@ -11,8 +14,8 @@ class leastSquaresModel:
         for i in range(2,p+1):
             Z = np.hstack((Z,np.power(X,i)))
         # Sine manipulation on features
-        for i in range(2,k+1):
-            Z = np.hstack((Z,np.sin(X*i)))
+        # for i in range(2,k+1):
+        #     Z = np.hstack((Z,np.sin(X*i)))
         self.w = np.linalg.solve(Z.T@Z,Z.T@y)
         self.p = p
         self.k = k
@@ -25,6 +28,37 @@ class leastSquaresModel:
         for i in range(2,self.p+1):
             Z = np.hstack((Z,np.power(Xhat,i)))
         # Sine manipulation on features
-        for i in range(2,self.k+1):
-            Z = np.hstack((Z,np.sin(Xhat*i)))
+        # for i in range(2,self.k+1):
+        #     Z = np.hstack((Z,np.sin(Xhat*i)))
         return Z@self.w
+
+# Simple regression using Open as parameter and Adj close as output
+# This is bad as it requires the opening price of today to predict todays close
+def linearOLS(df):
+	y = df['Adj Close']
+	# X = np.linspace(1,len(y),len(y))
+	# Using Open price as the features
+	X = df['Open']
+	X = np.array(X).reshape(len(X),1)
+	y = np.array(y).reshape(len(y),1)
+	slice = round(len(X)/2)
+	Xtrain = X[0:slice]
+	ytrain = y[0:slice]
+	Xtest = X[slice:len(X)]
+	ytest = y[slice:len(y)]
+	regress = leastSquaresModel(Xtrain,ytrain,3,5)
+	yhat_train = regress.predict(Xtrain)
+	# print((yhat_train - ytrain))
+	testError = E((yhat_train - ytrain)**2)
+	print("Training Error =", testError)
+	yhat = regress.predict(Xtest)
+	testError = E((yhat - ytest)**2)
+	print("Testing Error =", testError)
+	yplot = np.vstack((yhat_train, yhat))
+	plt.plot(df['Date'], y, label='Stock Price', color='blue')
+	# Plots Training
+	plt.plot(df['Date'][:slice], yhat_train, label='Training', color='green')
+	# Plots Testing
+	plt.plot(df['Date'][slice:], yhat, label='Testing', color='red')
+	plt.legend()
+	plt.show()
--- main.py
+++ main.py
@@ -5,48 +5,52 @@ import statsmodels.api as sm
 from misc import *
 import leastSquares as lsModel
 import numpy as np
+from sklearn import kernel_ridge
 
 def main():
-
 	# Load all asx200 companies as csv files
 	# df_asx200 = pd.read_csv('asx200_list.csv')
 	# for code in df_asx200['Code']:
 	# 	tmp = backtest_database(code + '.AX','2018-01-30','2019-08-11',1)
 	# 	tmp.create_csv()
 
-	ANZ = backtest_database('ANZ.AX','2018-01-30','2019-08-11',1)
-	df_ANZ = ANZ.read_csv()
-	linearOLS(df_ANZ)
+	stock = backtest_database('A2M.AX','2018-01-30','2019-08-11',1)
+	df_stock = stock.read_csv()
+	# lsModel.linearOLS(df_stock)
+	# print(df_stock)
+	df = load_df(df_stock)
 
+	# Trying some random shit with scikitlearn
+	# Xtrain = df[0:10]
+	# ytrain = df['Adj Close'][10:20]
+	# Xvalid = df[20:30]
+	# yvalid = df['Adj Close'][30:40]
+	# model = kernel_ridge.KernelRidge(alpha=1)
+	# model.fit(Xtrain, ytrain)
+	# abc = model.predict(Xtrain)
+	# yhat = model.predict(Xvalid)
+	# print(yhat)
+	# print(yvalid)
+	# plt.plot(df_stock['Date'][15:55], df_stock['Adj Close'][15:55], label='Actual Close Price', color='blue')
+	# plt.plot(df_stock['Date'][45:55],yhat,label='Predicted Close price', color='red')
+	# plt.legend()
+	# plt.show()
 
-# Simple regression using date as parameter and Adj close as output\
-def linearOLS(df):
-	y = df['Adj Close']
-	X = np.linspace(1,len(y),len(y))
-	X = np.array(X).reshape(len(X),1)
-	y = np.array(y).reshape(len(y),1)
-	slice = round(len(X)/2)
-	Xtrain = X[0:slice]
-	ytrain = y[0:slice]
-	Xtest = X[slice:len(X)]
-	ytest = y[slice:len(y)]
-	regress = lsModel.leastSquaresModel(Xtrain,ytrain,3,5)
-	yhat_train = regress.predict(Xtrain)
-	# print((yhat_train - ytrain))
-	testError = E((yhat_train - ytrain)**2)
-	print("Training Error =", testError)
-	yhat = regress.predict(Xtest)
-	testError = E((yhat - ytest)**2)
-	print("Training Error =", testError)
-	yplot = np.vstack((yhat_train, yhat))
-	plt.plot(df['Date'], y, label='Stock Price', color='blue')
-	# Plots Training
-	plt.plot(df['Date'][:slice], yhat_train, label='Training', color='green')
-	# Plots Testing
-	# plt.plot(df['Date'][slice:], yhat, label='Testing', color='red')
-	plt.legend()
-	plt.show()
+# Splits df to training data and y is a 60x15 matrix
+def load_df(df_stock,span=15,train=60):
+	Xtrain = pd.DataFrame()
+	ytrain = pd.DataFrame(columns=range(15))
+	# Xvalid = pd.DataFrame()
+	# yvalid = pd.DataFrame()
+	# df['Date'] = df_stock.Date[15:]
+	Xtrain['Open'] = df_stock.Open[span:train+span]
+	Xtrain['Adj Close'] = df_stock['Adj Close'][span:train+span]
+	Xtrain['Volume'] = df_stock.Volume[span:train+span]
+	Xtrain['15 day ewm'] = df_stock['Adj Close'].ewm(span=span, adjust=False).mean()[span:train+span]
+	for i in range(1,train):
+		ytrain.loc[i] = df_stock['Adj Close'][span+i:2*span+i].values
 
+	return Xtrain
 
 if __name__ == "__main__":
 	main()
