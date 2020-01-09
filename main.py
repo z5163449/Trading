a0a0fbf806a6a3db5b6efcaa52e5b7b36f797e94
Jimmy <z5163448@cse.unsw.edu.au>
2020-01-09 15:28:10 +0800

    got kernel ridge regression working (L2 regularised polynomial regression)

--- main.py
+++ main.py
@@ -18,39 +18,50 @@ def main():
 	df_stock = stock.read_csv()
 	# lsModel.linearOLS(df_stock)
 	# print(df_stock)
-	df = load_df(df_stock)
+	Xtrain,Xvalid,ytrain,yvalid = load_df(df_stock,ydays=30,valid=1)
 
 	# Trying some random shit with scikitlearn
 	# Xtrain = df[0:10]
 	# ytrain = df['Adj Close'][10:20]
 	# Xvalid = df[20:30]
 	# yvalid = df['Adj Close'][30:40]
-	# model = kernel_ridge.KernelRidge(alpha=1)
-	# model.fit(Xtrain, ytrain)
-	# abc = model.predict(Xtrain)
-	# yhat = model.predict(Xvalid)
-	# print(yhat)
-	# print(yvalid)
-	# plt.plot(df_stock['Date'][15:55], df_stock['Adj Close'][15:55], label='Actual Close Price', color='blue')
-	# plt.plot(df_stock['Date'][45:55],yhat,label='Predicted Close price', color='red')
-	# plt.legend()
-	# plt.show()
+	model = kernel_ridge.KernelRidge(alpha=1)
+	model.fit(Xtrain, ytrain)
+	abc = model.predict(Xtrain)
+	yhat = model.predict(Xvalid)
+	print(yhat)
+	print(yvalid)
+	print(df_stock['Adj Close'][76])
+	plt.plot(df_stock['Date'][15:106], df_stock['Adj Close'][15:106], label='Actual Close Price', color='blue')
+	plt.plot(df_stock['Date'][76:106],yhat.T,label='Predicted Close price', color='red')
+	plt.legend()
+	plt.show()
 
 # Splits df to training data and y is a 60x15 matrix
-def load_df(df_stock,span=15,train=60):
+# Matrix y contains the adj close of the next 15 days as this is what we want to predict
+def load_df(df_stock,span=15,train=60,valid=15,ydays=15):
 	Xtrain = pd.DataFrame()
-	ytrain = pd.DataFrame(columns=range(15))
-	# Xvalid = pd.DataFrame()
-	# yvalid = pd.DataFrame()
-	# df['Date'] = df_stock.Date[15:]
+	ytrain = pd.DataFrame(columns=range(ydays))
+	Xvalid = pd.DataFrame()
+	yvalid = pd.DataFrame(columns=range(ydays))
+
+	# from span days to trian + span days since we need at least span number of days for ewm
 	Xtrain['Open'] = df_stock.Open[span:train+span]
 	Xtrain['Adj Close'] = df_stock['Adj Close'][span:train+span]
 	Xtrain['Volume'] = df_stock.Volume[span:train+span]
-	Xtrain['15 day ewm'] = df_stock['Adj Close'].ewm(span=span, adjust=False).mean()[span:train+span]
-	for i in range(1,train):
-		ytrain.loc[i] = df_stock['Adj Close'][span+i:2*span+i].values
+	Xtrain['{} day ewm'.format(span)] = df_stock['Adj Close'].ewm(span=span, adjust=False).mean()[span:train+span]
 
-	return Xtrain
+	for i in range(1,train+1):
+		ytrain.loc[i] = df_stock['Adj Close'][span+i:span+ydays+i].values
+
+	Xvalid['Open'] = df_stock.Open[train+span:train+span+valid]
+	Xvalid['Adj Close'] = df_stock['Adj Close'][span+train:train+span+valid]
+	Xvalid['Volume'] = df_stock.Volume[span+train:train+span+valid]
+	Xvalid['15 day ewm'] = df_stock['Adj Close'].ewm(span=span, adjust=False).mean()[span+train:train+span+valid]
+
+	for i in range(1,valid+1):
+		yvalid.loc[i] = df_stock['Adj Close'][span+train+i:span+ydays+train+i].values
+	return Xtrain,Xvalid,ytrain,yvalid
 
 if __name__ == "__main__":
 	main()
